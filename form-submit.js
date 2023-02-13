84f36ecdfc2467f20a5783e29973120016719d4c
extrajake <extrajelly@live.ca>
2023-02-14 02:42:00 +0800

    added latest review feature

--- form-submit.js
+++ form-submit.js
@@ -2,111 +2,154 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get("id");
 var data = localStorage.getItem("data");
-
+var user_name = "";
 
 var headers = {
-  "QB-Realm-Hostname": "gosales.quickbase.com",
-  Authorization: "b4zk43xsngt3xd7ximtbdbxycvc",
-  "QB-App-Token": "b4zk43xsngt3xd7ximtbdbxycvc",
-  "Content-Type": "application/json"
+    "QB-Realm-Hostname": "gosales.quickbase.com",
+    Authorization: "b4zk43xsngt3xd7ximtbdbxycvc",
+    "QB-App-Token": "b4zk43xsngt3xd7ximtbdbxycvc",
+    "Content-Type": "application/json"
 };
 var body = {
-  from: "bsazkzsm2",
-  // from2: "bsa26ztqb",
-  select: [3, 6, 7, 8, 15, 16, 17, 21, 33, 42, 45, 48, 49],
-  where: "{42.EX.'" + id + "'}"
+    from: "bsazkzsm2",
+    // from2: "bsa26ztqb",
+    select: [3, 6, 7, 8, 15, 16, 17, 21, 33, 42, 45, 48, 49],
+    where: "{42.EX.'" + id + "'}"
 };
 
-// var body2 = {
-//   // from: "bsazkzsm2",
-//   from2: "bsa26ztqb",
-//   select: [17],
-//   where: "{42.EX.'" + id + "'}"
-// };
 
 const xmlHttp = new XMLHttpRequest();
 
 xmlHttp.open("POST", "https://api.quickbase.com/v1/records/query", true);
 for (const key in headers) {
-  xmlHttp.setRequestHeader(key, headers[key]);
+    xmlHttp.setRequestHeader(key, headers[key]);
 }
 
 xmlHttp.onreadystatechange = function() {
-  if (xmlHttp.readyState === XMLHttpRequest.DONE) {
-    const response = JSON.parse(xmlHttp.response);
-    if (response.data) {
-      console.log(response.data);
-      var item = response.data[0];
-      var name = item[6].value ? item[6].value.name : "";
-      var comment = item[8].value ? item[8].value : "";
-      // var rev = item[17].value ? item[17].value : "";
-      var validReview = item[17].value ? item[17].value : "";
-      var image = item[21].value ? item[21].value : "";
-      var repid = item[33].value ? item[33].value : "";
-      var avg = item[48].value ? item[48].value.toFixed(2) : "";
-      var avgCopy = item[49].value ? item[49].value : "";
-      
-      console.log(typeof avg, repid,);
-      if (item[7].value) {
-        document.getElementById("slaes_rep").innerHTML = item[7].value;
-        if (item[45].value == "Active") {
-          document.getElementById("slaes_rep").style.background = "#35a0c7";
-        } else if (item[45].value == "Inactive") {
-          document.getElementById("slaes_rep").style.background = "red";
+    if (xmlHttp.readyState === XMLHttpRequest.DONE) {
+        const response = JSON.parse(xmlHttp.response);
+        if (response.data) {
+            var item = response.data[0];
+            var name = item[6].value ? item[6].value.name : "";
+            var comment = item[8].value ? item[8].value : "";
+            // var rev = item[17].value ? item[17].value : "";
+            var validReview = item[17].value ? item[17].value : "";
+            var image = item[21].value ? item[21].value : "";
+            var repid = item[33].value ? item[33].value : "";
+            var avg = item[48].value ? item[48].value.toFixed(2) : "";
+            var avgCopy = item[49].value ? item[49].value : "";
+            user_name = name;
+            if (item[7].value) {
+                document.getElementById("slaes_rep").innerHTML = item[7].value;
+                if (item[45].value == "Active") {
+                    document.getElementById("slaes_rep").style.background = "#35a0c7";
+                } else if (item[45].value == "Inactive") {
+                    document.getElementById("slaes_rep").style.background = "red";
+                }
+            } else {
+                document.getElementById("slaes_rep").style.display = "none";
+            }
+            document.getElementById("rep-img").innerHTML = image;
+            // document.getElementById("name").value = name;
+            document.getElementById("rating_name").innerText = name;
+            document.getElementById("rep-name").innerHTML = name;
+            document.getElementById("avg").innerHTML = avg;
+            // document.getElementById("avg").innerHTML = avgCopy;
+            document.getElementById("repid").value = repid;
+            var headerss = {
+                "QB-Realm-Hostname": "gosales.quickbase.com",
+                Authorization: "QB-USER-TOKEN b5dge5_ph4g_0_c9g7fwvu96r3ucm42sf6dzpc36s",
+                "Content-Type": "application/json"
+            };
+
+            var body3 = {
+                from: "bsa26ztqb",
+                sortBy: [{
+                    "fieldId": 6,
+                    "order": "DESC"
+                }],
+                where: "{14.EX.'" + repid + "'}AND{17.EX.true}"
+            };
+            const xmlHttp3 = new XMLHttpRequest();
+            xmlHttp3.open("POST", "https://api.quickbase.com/v1/records/query", true);
+            for (const key in headerss) {
+                xmlHttp3.setRequestHeader(key, headerss[key]);
+            }
+
+            xmlHttp3.onreadystatechange = function() {
+                if (xmlHttp3.readyState === XMLHttpRequest.DONE) {
+                    const response = JSON.parse(xmlHttp3.response);
+                    if (response.data) {
+                        console.log(response.data);
+                        if (response.data.length > 0) {
+                            $(".last_reivew_box").show();
+                            var customer_html = '<span>&ldquo;</span>' + response.data[0][8]['value'] + '<span>&rdquo;</span>';
+                            $(".last_review_comment").html(customer_html);
+                            $("#customer_name").text(response.data[0][9]['value']);
+                            var html = "";
+                            for (i = 1; i < response.data.length; i++) {
+                                html += '<div class="row py-1" style="border-bottom:1px solid #eee">';
+                                html += '<div class="col-8">' + response.data[i][9]['value'] + '</div>';
+                                html += '<div class="col-4 text-right">';
+                                html += '<small style="font-style: italic;color: gray;">' + response.data[i][6]['value'] + '</small>';
+                                html += '</div>';
+                                html += '<div class="col-md-12" style="color: gray;">&ldquo;' + response.data[i][8]['value'] + '&rdquo;</div>';
+                                html += '</div>';
+                            }
+                            if (html) {
+                                $("#other_review_box").html(html);
+                            }
+                        }
+
+                    }
+                }
+            };
+
+            xmlHttp3.send(JSON.stringify(body3));
+
         }
-      } else {
-        document.getElementById("slaes_rep").style.display = "none";
-      }
-      document.getElementById("rep-img").innerHTML = image;
-      document.getElementById("name").value = name;
-      document.getElementById("rating_name").innerText = name;
-      document.getElementById("rep-name").innerHTML = name;
-      document.getElementById("avg").innerHTML = avg;
-      // document.getElementById("avg").innerHTML = avgCopy;
-      document.getElementById("repid").value = repid;
-      // document.getElementById("revTxt").value = rev;
     }
-    // console.log(rev)
-  }
 };
 
 xmlHttp.send(JSON.stringify(body));
 
 var headers = {
-  "QB-Realm-Hostname": "gosales.quickbase.com",
-  Authorization: "b4zk43xsngt3xd7ximtbdbxycvc",
-  "QB-App-Token": "b4zk43xsngt3xd7ximtbdbxycvc",
-  "Content-Type": "application/json"
+    "QB-Realm-Hostname": "gosales.quickbase.com",
+    Authorization: "b4zk43xsngt3xd7ximtbdbxycvc",
+    "QB-App-Token": "b4zk43xsngt3xd7ximtbdbxycvc",
+    "Content-Type": "application/json"
 };
 
 var body = {
-  from: "bsa26ztqb",
-  // from2: "bsa26ztqb",
-  select: [3, 6, 7, 8, 15, 16, 17],
-  where: "{42.EX.'" + id + "'}"
+    from: "bsa26ztqb",
+    // from2: "bsa26ztqb",
+    select: [3, 6, 7, 8, 15, 16, 17],
+    where: "{42.EX.'" + id + "'}"
 };
 
 const xmlHttp2 = new XMLHttpRequest();
 
-console.log(xmlHttp2);
 
 xmlHttp2.open("POST", "https://api.quickbase.com/v1/records/query", true);
 for (const key in headers) {
-  xmlHttp2.setRequestHeader(key, headers[key]);
+    xmlHttp2.setRequestHeader(key, headers[key]);
 }
 
 xmlHttp2.onreadystatechange = function() {
-  if (xmlHttp2.readyState === XMLHttpRequest.DONE) {
+    if (xmlHttp2.readyState === XMLHttpRequest.DONE) {
 
-    const response = JSON.parse(xmlHttp2.response);
+        const response = JSON.parse(xmlHttp2.response);
 
-    if (response.data) {
-      console.log(xmlHttp2.response);
-      var item2 = response.data[0];
-      var name2 = item2[6].value ? item2[6].value.name2 : "";
-      document.getElementById("latest-review").innerHTML = name2;
-   }
-}};
+        if (response.data) {
+            console.log(xmlHttp2.response);
+            var item2 = response.data[0];
+            var name2 = item2[6].value ? item2[6].value.name2 : "";
+            document.getElementById("latest-review").innerHTML = name2;
+
+        }
+    }
+};
+
 
 // xmlHttp.open("POST", "https://api.quickbase.com/v1/records/query", true);
 // for (const key in headers) {
@@ -148,82 +191,78 @@ xmlHttp2.onreadystatechange = function() {
 
 
 $("#testform").on("submit", function(e) {
-  e.preventDefault();
-  console.log(e);
-  var email = $("#email").val();
-  var name = $("#name").val();
-  var phone = $("#phone").val();
-  var comments = $("#comments").val();
-  var repid = $("#repid").val();
-  var stars = $("input[name='stars']:checked").val();
-  if (stars == null) {
-    swal({
-      title: "warning",
-      text: "Select review stars.",
-      icon: "warning"
-    });
-  } else {
-    var headers = {
-      "QB-Realm-Hostname": "gosales.quickbase.com",
-      Authorization: "QB-USER-TOKEN b5dge5_ph4g_0_c9g7fwvu96r3ucm42sf6dzpc36s",
-      "Content-Type": "application/json"
-    };
-    var body = {
-      to: "bsa26ztqb",
-      data: [
-        {
-          "7": {
-            value: stars
-          },
-          "14": {
-            value: repid
-          },
-          "9": {
-            value: name
-          },
-          "10": {
-            value: phone
-          },
-          "11": {
-            value: email
-          },
-          "8": {
-            value: comments
-          }
+    e.preventDefault();
+    var email = $("#email").val();
+    var name = $("#name").val();
+    var phone = $("#phone").val();
+    var comments = $("#comments").val();
+    var repid = $("#repid").val();
+    var stars = $("input[name='stars']:checked").val();
+    if (stars == null) {
+        swal({
+            title: "warning",
+            text: "Select review stars.",
+            icon: "warning"
+        });
+    } else {
+        var headers = {
+            "QB-Realm-Hostname": "gosales.quickbase.com",
+            Authorization: "QB-USER-TOKEN b5dge5_ph4g_0_c9g7fwvu96r3ucm42sf6dzpc36s",
+            "Content-Type": "application/json"
+        };
+        var body = {
+            to: "bsa26ztqb",
+            data: [{
+                "7": {
+                    value: stars
+                },
+                "14": {
+                    value: repid
+                },
+                "9": {
+                    value: name
+                },
+                "10": {
+                    value: phone
+                },
+                "11": {
+                    value: email
+                },
+                "8": {
+                    value: comments
+                }
+            }],
+            fieldsToReturn: [7]
+        };
+        xmlHttp.open("POST", "https://api.quickbase.com/v1/records", true);
+        for (const key in headers) {
+            xmlHttp.setRequestHeader(key, headers[key]);
         }
-      ],
-      fieldsToReturn: [7]
-    };
 
-    xmlHttp.open("POST", "https://api.quickbase.com/v1/records", true);
-    for (const key in headers) {
-      xmlHttp.setRequestHeader(key, headers[key]);
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState === XMLHttpRequest.DONE) {
+                const response = JSON.parse(xmlHttp.response);
+
+                if (response.metadata) {
+                    if (response.metadata.createdRecordIds[0]) {
+                        swal({
+                            title: "Success",
+                            text: "Review submitted successfully!",
+                            icon: "success"
+                        }).then(function(result) {
+                            location.reload(true);
+                        });
+                    }
+                } else {
+                    swal({
+                        title: "Error",
+                        text: response.message,
+                        icon: "error"
+                    });
+                }
+            }
+        };
+        xmlHttp.send(JSON.stringify(body));
+
     }
-
-    xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState === XMLHttpRequest.DONE) {
-        const response = JSON.parse(xmlHttp.response);
-
-        if (response.metadata) {
-          if (response.metadata.createdRecordIds[0]) {
-            swal({
-              title: "Success",
-              text: "Review submitted successfully!",
-              icon: "success"
-            }).then(function(result) {
-              location.reload(true);
-            });
-          }
-        } else {
-          swal({
-            title: "Error",
-            text: response.message,
-            icon: "error"
-          });
-        }
-      }
-    };
-    xmlHttp.send(JSON.stringify(body));
-    
-  }
 });
