3a6870aa0f7878f03553480512be8353c1a0b653
extrajake <extrajelly@live.ca>
2023-03-27 19:00:01 +0700

    blended phone email in contcat form

--- form-callback.js
+++ form-callback.js
@@ -62,9 +62,6 @@ $("#testform").on("submit", function(e) {
         "8": {
           value: email
         },
-        "9": {
-          value: phone
-        },
         "15": {
           value: rid
         }
